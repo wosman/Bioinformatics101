f078bbf9a840fbc464956971b559af89ce30603b
wosman <waleed.a.osman@gmail.com>
2016-09-21 12:39:04 -0400

    Promted user.

--- DNA2RNA.py
+++ DNA2RNA.py
@@ -1,22 +1,18 @@
 from termcolor import colored
-import sys
-
-# print colored('hello', 'red'), colored('world', 'green')
-
-
 
 # This program will transcribe a sequence of DNA entered by the user into RNA
 # You will have to pip install termcolor
+
 
 def transcribe(dna):
     print colored('Attempting to transcribe your deoxyribonucleic acid sequence into a ribonucleic acid sequence.', 'yellow')
     return dna.replace('T','U')
 
-print colored('Please enter your sequence of DNA:', 'blue')
-print colored('e.g. ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCCCTGCC', 'magenta')
+print colored('Please enter your sequence of DNA below:', 'blue')
+print colored('e.g. (exempli gratia) ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCCCTGCC', 'magenta')
 dna = raw_input()
+# dna = 'TGCCATTGTCCCCCGGCCTTTTTTTTTT'
 rna = ''
 rna = transcribe(dna)
 print colored('Your transcribed sequence is:', 'red') + colored(rna, 'green')
 print colored('Thank you', 'cyan', attrs=['reverse','blink'])
-
